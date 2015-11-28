e2809a460c3167b34b0f9d5180a0306d1c9d26ea
Matthew Lake <mgtlake@outlook.com>
2015-11-28 11:40:35 +1000

    set up request

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 var express = require("express");
 var app = express();
+var fs = require("fs");
+var request = require("request");
+
+var key = '';
+var server;
 
 app.get('/', function(req, res) {
-	res.send("Hello World!");
+	var url = "http://academic.research.microsoft.com/json.svc/";
+
+	request(url, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			res.send(body);
+		}
+	});
 });
 
-var server = app.listen(3000, function() {
-	var host = server.address().address;
-	var port = server.address().port;
+fs.readFile("API key.txt", "utf8" , function(err, data) {
+	key = data;
+	console.log(key);
 
-	console.log("Example app listening at http://%s:%s", host, port);
+	server = app.listen(3000, function() {
+		var host = server.address().address;
+		var port = server.address().port;
+
+		console.log("Example app listening at http://%s:%s", host, port);
+	});
 });
