4f902e2e196727810a472612bdb81ba0b03f4dfd
Matthew Lake <mgtlake@outlook.com>
2015-11-28 14:19:06 +1000

    query api

--- index.js
+++ index.js
@@ -7,23 +7,36 @@ var key = '';
 var server;
 
 app.get('/', function(req, res) {
+	var author = req.query["me"] ? req.query["me"] : "";
+
 	var url = "http://academic.research.microsoft.com/json.svc/";
+
+	url += "search?";
+
+	url += "AppId=" + key;
+
+	url += "&FullTextQuery=" + author.replace(' ', '+');
+	url += "&ResultObjects=" + "author";
+	url += "&StartIdx=" + "1";
+	url += "&EndIdx=" + "1";
 
 	request(url, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
-			res.send(body);
+			var json = JSON.parse(body);
+
+			if (json["d"]["Author"]["Result"] === null) {
+				console.log("error retrieving author");
+			} else {
+				console.log(json["d"]["Author"]["Result"][0]["HIndex"]);
+			}
+
+			res.send(JSON.stringify(json, null, 4));
 		}
 	});
 });
 
 fs.readFile("API key.txt", "utf8" , function(err, data) {
 	key = data;
-	console.log(key);
 
-	server = app.listen(3000, function() {
-		var host = server.address().address;
-		var port = server.address().port;
-
-		console.log("Example app listening at http://%s:%s", host, port);
-	});
+	server = app.listen(3000);
 });
